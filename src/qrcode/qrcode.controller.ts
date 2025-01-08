1c6a8fe5b335958fa3dddbd23d5bed764e9aa80f
Ramofvm <ramonfhelipe547@gmail.com>
2025-01-07 22:09:11 -0300

    fazendo o return do qrcode

--- src/qrcode/qrcode.controller.ts
+++ src/qrcode/qrcode.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Post, Query } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { QrcodeService } from './qrcode.service';
 
 @Controller('qrcode')
 export class QrcodeController {
+    constructor(private qrcodeService: QrcodeService) {}
 
-    constructor( private qrcodeService:QrcodeService){}
     @Post('generater')
-    async GeneratorQrcode(@Query('data') data: string) {
-        
-        if (!data) {
-            throw new Error('Requer um parâmetro');
+    async GeneratorQrcode(@Body() body: { data: string }) {
+       
+        if (!body.data) {
+            throw new Error('Requer um parâmetro "data"');
         } else {
            
-            const qrCode = await this.qrcodeService.GerarQr(data);
+            const qrCode = await this.qrcodeService.GerarQr(body.data);
             return { qrcode: qrCode };
         }
     }
